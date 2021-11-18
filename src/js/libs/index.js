6462b942f28ec8f2173648ab91c3820a24606cc6
unknown <shishiv30@live.cn>
2021-11-17 21:29:56 -0800

    update css

--- src/js/libs/index.js
+++ src/js/libs/index.js
@@ -1,4 +1,4 @@
 import './_jquery.datetimepicker.lib';
-import './_jquery.megalist'
-import './_jquery.wheel.lib'
-import './_nouislider.lib'
+// import './_jquery.megalist'
+import './_jquery.wheel.lib';
+import './_nouislider.lib';
